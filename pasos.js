635e9aff551a2d31b2f25036538cde4d15472a67
Iván García Rodríguez <ivan.garcia.rodriguez@hotmail.es>
2022-09-03 19:06:08 +0200

    Preparado el esquema básico de la página principal
    Faltan terminar de ajustar los tipos css de los
    artículos.
    Header preparado en un archivo aparte e incluido
    en la vista ejs del index.
    Lo siguiente a hacer es preparar la paginación y
    comprobar que funciona adecuadamente, aunque lo
    haga con un array simulado por el momento.

---
+++ pasos.js
@@ -0,0 +1,25 @@
+//DONE Crear app express y probarla
+  //Instalado ESLINT
+  //Instalado NODEMON en modo desarrollo
+  //Creados scripts de arranque básicos
+    //Desarrollo en windows
+    //Desarrollo en linux
+    //Arranque de la aplicación
+
+//TODO Instalar mongoose
+
+//TODO Instalar el modelo de anuncios y probarlo
+
+//TODO Script de inicialización de la base de datos y probarlo
+
+//TODO README.md con las instrucciones de uso --> en curso
+
+//TODO Primera versión básica de la API
+
+//TODO Crear la página básica del site
+  //TODO mostrar una lista de anuncios
+
+//TODO Mejorar la vista de anuncios
+  //TODO Paginación
+  //TODO Filtros
+
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+router.get('/', (req, res, next) => {
+  res.render('index', {
+    title:"Nodepop - The Web for purchase-sale second-hand articles",
+    ads:[1,2,3,4,5]
+  });
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
